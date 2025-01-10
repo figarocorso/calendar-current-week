af2db1b4520cf659d77685bb36bbae5465fa8ae5
Miguel Julián <figarocorso@users.noreply.github.com>
2025-01-10 21:34:53 +0100

    Fix get week number logic (#4)

--- change_background.js
+++ change_background.js
@@ -1,18 +1,11 @@
 Date.prototype.getWeek = function(){
-    // From stackoverflow: https://stackoverflow.com/a/9781448
-    var day_miliseconds = 86400000,
-        onejan = new Date(this.getFullYear(),0,1,0,0,0),
-        onejan_day = (onejan.getDay()==0) ? 7 : onejan.getDay(),
-        days_for_next_monday = (8-onejan_day),
-        onejan_next_monday_time = onejan.getTime() + (days_for_next_monday * day_miliseconds),
-        first_monday_year_time = (onejan_day>1) ? onejan_next_monday_time : onejan.getTime(),
-        this_date = new Date(this.getFullYear(), this.getMonth(),this.getDate(),0,0,0),// This at 00:00:00
-        this_time = this_date.getTime(),
-        days_from_first_monday = Math.round(((this_time - first_monday_year_time) / day_miliseconds));
-
-    var first_monday_year = new Date(first_monday_year_time);
-
-    return (days_from_first_monday>=0 && days_from_first_monday<364) ? Math.ceil((days_from_first_monday+1)/7) : 52;
+    const dayInMiliseconds = 86400000;
+    const firstOfJanuary = new Date(this.getFullYear(), 0, 1);
+    const dateAtMidnight = new Date(this.getFullYear(), this.getMonth(), this.getDate());
+    const daysSinceFirstOfJan = ((dateAtMidnight - firstOfJanuary) / dayInMiliseconds) + 1;
+    const firstOfJanuaryWeekday = firstOfJanuary.getDay() ? firstOfJanuary.getDay() : 7;
+    const offset = firstOfJanuaryWeekday - 1;
+    return Math.ceil((daysSinceFirstOfJan + offset) / 7);
 }
 
 function getHeaderElement() {
